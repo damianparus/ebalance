1f4fd9aee7b7b0a98cb2cb1d859627ec1c4431f6
Damian Parus <damian.parus@allegro.pl>
2021-03-20 15:35:25 +0100

    init

---
+++ src/main/kotlin/com/indy/ebalance/EBalanceApplication.kt
@@ -0,0 +1,11 @@
+package com.indy.ebalance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EBalanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<EBalanceApplication>(*args)
+}
