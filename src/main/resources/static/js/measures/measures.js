4ec0a80f0c3c4301ab01936a93e7d532e8016844
Damian Parus <damian.parus@allegro.pl>
2021-07-03 21:17:16 +0200

    cimple chart

--- src/main/resources/static/js/measures/measures.js
+++ src/main/resources/static/js/measures/measures.js
@@ -3,26 +3,58 @@
 const e = React.createElement;
 
 class LikeButton extends React.Component {
+
     constructor(props) {
-        super(props);
-        this.state = { liked: false };
+        super();
+    }
+
+    componentDidMount() {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     }
 
     render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
-        );
+        return e('canvas', {id: 'myChart', width: '100px', height: '100px'}, '');
     }
 }
 
 
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(
+    e(LikeButton),
+    document.querySelector('#chartDiv')
+);
 
